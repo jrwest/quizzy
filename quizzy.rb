a7deb5c06330b41487f728f369f4ad147dca664d
jrwest <jordan.west@westwardwd.com>
2010-06-12 00:24:03 -0400

    replaced generated root route with a view
    added HAML to application. Views are stored in ./views/

--- quizzy.rb
+++ quizzy.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
+require 'haml'
 
 class Quizzy < Sinatra::Base
+  set :views, File.join(File.dirname(__FILE__), 'views')
+
   get '/' do
-    'Hello Quizzy!'
+    haml :index
   end
 end
